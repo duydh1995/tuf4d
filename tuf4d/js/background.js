0cd3f18aec0a6a3ac9a11a4c6ad6cdc88793d482
Noname <idonthave@email.com>
2017-10-10 02:06:31 +0700

    Initialize

---
+++ tuf4d/js/background.js
@@ -0,0 +1,24 @@
+var
+   contexts = ["all", "page", "frame", "selection", "link", "editable", "image", "video", "audio"];
+
+chrome.contextMenus.create({
+   "title": "tuf4d",
+   "contexts": contexts,
+   "onclick": onClick
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+   onClick({}, tab);
+});
+
+function onClick(info, tab) {
+   var
+      url = tab.url;
+
+   if (url.match("^http://dictionary.cambridge.org/")) {
+      var
+         message = "cambridge_get_mp3_url";
+
+      chrome.tabs.sendMessage(tab.id, message);
+   }
+}
---
+++ tuf4d/js/content.js
@@ -0,0 +1,29 @@
+﻿chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+   if (message === "cambridge_get_mp3_url") {
+      var
+         $buttons = $(".audio_play_button.uk");
+
+      if ($buttons.length > 0) {
+         var
+            $button = $buttons.first(),
+            mp3Src = $button.attr("data-src-mp3");
+
+         copyToClipboard(mp3Src);
+
+         console.log(mp3Src);
+      } else {
+         console.log("NOT FOUND ANY BUTTONS");
+      }
+   }
+});
+
+function copyToClipboard(text) {
+   const input = document.createElement("input");
+   input.style.position = "fixed";
+   input.style.opacity = 0;
+   input.value = text;
+   document.body.appendChild(input);
+   input.select();
+   document.execCommand("Copy");
+   document.body.removeChild(input);
+};
